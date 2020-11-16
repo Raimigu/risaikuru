214fcd22739beb91e20d247dd69cd04d40c2a8b4
Raimigu <74287169+Raimigu@users.noreply.github.com>
2020-11-16 23:19:03 +0800

    2nd page

---
+++ app/src/main/java/meandrick/martin/front_page_risaikuru/front_page.java
@@ -0,0 +1,54 @@
+package meandrick.martin.front_page_risaikuru;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class front_page extends AppCompatActivity {
+    private ImageButton i4;
+    private ImageButton i6;
+    private ImageButton i5;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.front_page);
+i4 =(ImageButton) findViewById(R.id.i4);
+i6 =(ImageButton) findViewById(R.id.i6);
+i5 = (ImageButton) findViewById(R.id.i5);
+
+i4.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        openActivity2();
+    }
+});
+i6.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        openActivity3();
+    }
+});
+i5.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        openActivity4();
+    }
+});
+    }
+    public void openActivity2(){
+        Intent intent = new Intent(this, bottle_second_page.class);
+        startActivity(intent);
+    }
+    public void openActivity3(){
+        Intent intent = new Intent(this, paper_second_page.class);
+        startActivity(intent);
+    }
+    public void openActivity4(){
+        Intent intent = new Intent(this, cans_second_page.class);
+        startActivity(intent);
+    }
+}
